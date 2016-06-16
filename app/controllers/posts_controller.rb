60e31d2459d2237824d29f62524a9308c8acf400
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-16 12:02:11 +0200

    Load and authorize resource posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,18 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
 
   def index
     @posts = Post.order( created_at: :desc )
-    authorize! :read, @posts
   end
-
-
-  post_params = params.require( :post ).permit( :content )
 
   def create
-    post_params = params.require( :post ).permit( :content )
-
-    @post = Post.new( content: post_params[:content] )
-    @post.user = current_user
-    authorize! :create, @post
-   end
-
-   def create
-   # ...
-   @post = Post.new( content: post_params[:content] )
-   @post.user = current_user
-   # ...
+    if @post.save
+      redirect_to posts_path
+    else
+      render posts_path
+    end
   end
+
   def user
     @user = User.find( params[:user_id] )
     authorize! :read, @user
@@ -31,6 +22,12 @@ class PostsController < ApplicationController
 
     @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
     authorize! :read, @likes
+  end
+
+  private
+
+  def post_params
+    params.require( :post ).permit( :content )
   end
 
 end
