f3dda4a21a18c885dd0cce7911d1ba5aad0c663b
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-16 11:33:56 +0200

    Like posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-  resources :posts
+  resources :posts  do
+    resources :likes
+    end
 
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 
