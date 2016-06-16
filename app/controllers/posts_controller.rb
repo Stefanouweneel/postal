f3a2fcb4887de10b595696556ac818398cae9d34
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-16 11:35:53 +0200

    User likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,9 +22,11 @@ class PostsController < ApplicationController
    # ...
   end
   def user
-   @user = User.find( params[:user_id] )
+     @user = User.find( params[:user_id] )
 
-   @posts = Post.where( user: @user ).order( created_at: :desc )
+     @posts = Post.where( user: @user ).order( created_at: :desc )
+
+     @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :user
+  has_many :likes
 
   def handle
   self.email.split('@')[0]
